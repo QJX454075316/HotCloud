d7725d3db5482ee621f9e557aa873800c6bfcad3
QJX454075316 <quan19951105@163.com>
2019-09-20 15:22:15 +0800

    up

---
+++ HotEureka/src/test/java/com/qjc/hotEureka/HotEurekaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.qjc.hotEureka;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HotEurekaApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
