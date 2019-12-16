54a2fdd037d9e99c02a16891d994dadaeb2d990d
QJX454075316 <quan19951105@163.com>
2019-12-16 17:59:54 +0800

    up

---
+++ HotCommon/src/test/java/com/qjx/AppTest.java
@@ -0,0 +1,16 @@
+package com.qjx;
+
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+     */
+}
